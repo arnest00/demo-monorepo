0c8c4515badc1ee08d347b2bbb2e3889847646b9
Osmond Arnesto <osmond@heysparkbox.com>
2024-04-29 08:14:04 -0700

    feat: Add app `demo-app`
    - Initialize with `create-next-app@latest`

---
+++ apps/demo-app/src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Demo App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <h1>Hello world!</h1>
+      </main>
+    </>
+  );
+}
